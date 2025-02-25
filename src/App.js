cd4e69b723f3ebab7636903e6e385140c4484641
Aseem Gupta <iamgeekyaseem@gmail.com>
2025-02-26 03:48:44 +0530

    working to fix the search bug

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React from "react";
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
+
 import "./App.css";
 import SearchIcon from "./search.svg";
+import MovieCard from "./MovieCard";
 
 const API_URL = "https://www.omdbapi.com/?apikey=cef31aac";
 
-const movie1 = {
-  "Title": "Batman",
-  "Year": "1989",
-  "imdbID": "tt2586634",
-  "Poster": "N/A"
-}
 
 const App = () => {
+  const [movies, setMovies] = useState([]);
+
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`);
     const data = await response.json();
+    setMovies(data.search);
     console.log(data);
   };
 
   useEffect(() => {
-    searchMovies("batman");
+    searchMovies("Batman");
   }, []);
 
   return (
@@ -28,32 +27,27 @@ const App = () => {
       <h1>CineWorld</h1>
 
       <div className="search">
-        <input 
+        <input
           placeholder="Search Movies..."
-          value= "Superman"
+          value="Superman"
           onChange={() => {}}
-         />
-        <img 
-          src = {SearchIcon}
-          alt="Search"
-          onClick={() => {}}
         />
+        <img src={SearchIcon} alt="Search" onClick={() => {}} />
       </div>
 
-      <div className="container">
-        <div className="movie">
-          <div>
-            <p>{movie1.Year}</p>
-          </div>
-          <div>
-            <img src={movie1.Poster !== 'N/A' ? movie1.Poster :'https://placehold.co/400x600'} 
-            alt={movie1.Title}/>
-          </div>
+      {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => (
+            <MovieCard movie={movie} />
+          ))}
         </div>
-      </div>
-
+      ) : (
+        <div className="empty">
+          <h2>No Movies Found</h2>
+        </div>
+      )}
     </div>
-  )
+  );
 };
 
 export default App;
